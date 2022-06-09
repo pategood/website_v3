b599f79602e8273adac68e06a8498a771d988a6e
pategood <zxc2283879877@outlook.com>
2022-06-09 17:38:03 +0800

    fix: 引入大菠萝

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import { loadAllPlugins } from "./plugins";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 // 加载所有插件
 loadAllPlugins(app);
 
-app.use(store).use(router).mount("#app");
+app.use(router).use(createPinia()).mount("#app");
