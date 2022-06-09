448bf2c9f9636dd450c5cfeba629be71616c62ef
pategood <zxc2283879877@outlook.com>
2022-06-09 14:38:43 +0800

    fix: 工具类补充

---
+++ src/service/request.ts
@@ -0,0 +1,14 @@
+/**
+ * 封装 axios 请求模块
+ */
+// import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+// import { ResultEnum } from "./httpEnum.ts";
+
+// const config = {
+// 	// 默认地址
+// 	baseURL: import.meta.env.VITE_API_URL as string,
+// 	// 设置超时时间（10s）
+// 	timeout: ResultEnum.TIMEOUT as number,
+// 	// 跨域时候允许携带凭证
+// 	withCredentials: true
+// };
